b2f259856c709029658ecc5bd8cec84651ceecf0
zyongbo <zyongbo@users.noreply.github.com>
2015-04-15 22:49:49 -0600

    Create WordCount.java

---
+++ WordCount.java
@@ -0,0 +1,31 @@
+package leo.impl;
+
+import java.util.Scanner;
+
+import leo.adt.Entry;
+import leo.adt.Map;
+
+public class WordCount {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		Map<String, Integer> freq = new ChainHashMap<>();
+		Scanner doc = new Scanner(System.in).useDelimiter("[^a-zA-Z]+");
+		while(doc.hasNext()) {
+			String word = doc.next().toLowerCase();
+			Integer count = freq.get(word);
+			if(count==null)
+			count = 0;
+			freq.put(word, 1+count);
+		}
+		int maxCount = 0;
+		String maxWord = "no word";
+		for(Entry<String, Integer> ent : freq.entrySet())
+			if(ent.getValue()>maxCount) {
+				maxWord = ent.getKey();
+				maxCount = ent.getValue();
+			}
+		System.out.print("The most frequent word is " + maxWord);
+		System.out.println(" with " + maxCount + " occurrences");
+	}
+}
